fadd77f810b89a13761d4486b208635bfda194ed
Mridul <140313918+Mridul-23@users.noreply.github.com>
2025-01-12 19:51:42 +0530

    Some Adjustments

--- frontend/anisite/src/App.jsx
+++ frontend/anisite/src/App.jsx
@@ -4,14 +4,17 @@ import './App.css';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { Outlet } from 'react-router-dom';
 import useDynamicTitle from './hooks/useDynamicTitle.js';
+import ScrollToTop from './utils/scrollToTop.jsx';
 
 function App() {
   useDynamicTitle("ANIVERSE");
   return (
     <AuthProvider>
-      <Navbar />
-      <Outlet />
-      <Footer />
+      <ScrollToTop>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </ScrollToTop>
     </AuthProvider>
   )
 }
