9038f62abbf883f527c57b1462e3691eebd3fa08
Mridul <140313918+Mridul-23@users.noreply.github.com>
2025-02-12 18:25:34 +0530

    little change

--- backend/api/auth_api/urls.py
+++ backend/api/auth_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from . import views
 
 from rest_framework.permissions import BasePermission
@@ -9,6 +9,6 @@ from rest_framework.permissions import BasePermission
 urlpatterns =[
     path('login/', views.LoginView.as_view(), name='login'),
     path('register/', views.RegisterView.as_view(), name='register_user'),
-    # path('gettoken/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('gettoken/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
 ]
